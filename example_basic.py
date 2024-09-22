8ac2b63fbd5e816629bed4e8809cd7fec6b5cfa1
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-09-22 15:16:08 +0900

    Change order of RGB color in example_basic.py

--- example_basic.py
+++ example_basic.py
@@ -14,7 +14,8 @@ shape = screen.shape  # (height, width, 3)
 print(f"Screen size: {width}x{height}")
 
 # Create an image
-img_rgb = np.fromfunction(lambda y, x, c: 255 * (c == 0) + x / (width - 1) * 255 * (c == 1) + y / (height - 1) * 255 * (c == 2), (height, width, 3)).astype(np.uint8)
+# red -> x, green -> y, blue -> constant
+img_rgb = np.fromfunction(lambda y, x, c: 128 * (c == 2) + x / (width - 1) * 255 * (c == 0) + y / (height - 1) * 255 * (c == 1), (height, width, 3)).astype(np.uint8)
 
 # Display the image
 screen.imshow(img_rgb)
